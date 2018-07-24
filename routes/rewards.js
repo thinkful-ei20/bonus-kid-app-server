978097cbf7469f5c3a8c53b52aa688aa05fc7824
Isael Lizama <isaelwebdeveloper@gmail.com>
2018-07-24 07:17:44 -0600

    updated reward creation with parent

--- routes/rewards.js
+++ routes/rewards.js
@@ -7,43 +7,95 @@ const router = express.Router();
 const passport = require('passport');
 
 const Rewards = require('../models/rewards');
+const Parent = require('../models/parent');
+
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET, JWT_EXPIRY } = require('../config');
+
+//move to helper folder
+function createAuthToken(user) {
+  return jwt.sign({ user }, JWT_SECRET, {
+    subject: user.username,
+    expiresIn: JWT_EXPIRY
+  });
+}
 
 router.use('/', passport.authenticate('jwt', { session: false, failWithError: true }));
 
 //Create Parent Reward
 router.post('/', (req, res, next) => {
-  let { name, pointValue, purchased , day, hour} = req.body;
+  let { name, pointValue, purchased, day, hour } = req.body;
   const { id } = req.user;
-  if(!day) day = 0;
-  if(!hour) hour = 0;
+  let updatedRewards; 
+  let rewardTest;
+  if (!day) day = 0;
+  if (!hour) hour = 0;
+  let updateParent = {};
   Rewards.create({
     parentId: id,
     name,
     pointValue,
     purchased,
     currentTime: moment().valueOf(),
-    expiryDate: moment().add(day,'days').add(hour,'hours').valueOf()
+    expiryDate: moment().add(day, 'days').add(hour, 'hours').valueOf()
   })
     .then(reward => {
-      res.json(reward);
+      console.log('reward =>>', reward);
+      rewardTest = reward;
+      return Parent.findById(id)
+
+      // .catch((err) => console.log(err));
+    })
+    .then(parent => {
+      console.log('after reward => ',rewardTest)
+      updatedRewards = {rewards: [...parent.rewards, rewardTest.id]};
+      // updateParent.rewards = updatedRewards;
+      console.log('updatedRewards', updatedRewards);
+      console.log(true)
+      return;
+    })
+    .then(() => {
+      return Parent.findByIdAndUpdate({_id: id}, updatedRewards, { new: true })
+    })
+    .then((result) => {
+      console.log('before populate', result)
+      return Parent.findById(result.id)
+        .populate([{
+          path: 'child',
+          model: 'Child',
+          populate: {
+            path: 'tasks',
+            model: 'Tasks'
+          }
+        },
+        {
+          path: 'rewards',
+          model: 'Rewards'
+        }]);
+    })
+    .then((result) => {
+      console.log('result', result);
+      const authToken = createAuthToken(result);
+      return res.send({ authToken });
     })
     .catch(err => {
+      console.log(err);
       if (err.code === 11000) {
         let error = new Error('Same name for reward');
         error.status = 400;
         next(error);
       }
-      if(err.message === 'Rewards validation failed: name: Path `name` is required.'){
+      if (err.message === 'Rewards validation failed: name: Path `name` is required.') {
         let error = new Error('name is required');
         error.status = 400;
         next(error);
       }
-      if(err.message === 'Rewards validation failed: pointValue: Path `pointValue` is required.'){
+      if (err.message === 'Rewards validation failed: pointValue: Path `pointValue` is required.') {
         let error = new Error('pointValue are required');
         error.status = 400;
         next(error);
       }
-      
+
       next(err);
     });
 });
@@ -66,8 +118,8 @@ router.get('/', (req, res, next) => {
 //needs work
 router.get('/child', (req, res, next) => {
   const { parentId } = req.user;
-  console.log('xx',parentId);
-  
+  console.log('xx', parentId);
+
 
   Rewards.find({ parentId })
     .then(rewards => {
@@ -86,36 +138,36 @@ router.put('/:id', (req, res, next) => {
   day = parseInt(day);
   const updatedReward = {};
 
-  if(name){
+  if (name) {
     updatedReward.name = name;
   }
-  if (pointValue){
+  if (pointValue) {
     updatedReward.pointValue = pointValue;
-  } 
-  if (hour > 0 || day > 0){
-    console.log('this ran');
-    updatedReward.expiryDate = moment().add(day,'days').add(hour,'hours').valueOf();
   }
-  if (hour === 0 && day === 0){
+  if (hour > 0 || day > 0) {
+    console.log('this ran');
+    updatedReward.expiryDate = moment().add(day, 'days').add(hour, 'hours').valueOf();
+  }
+  if (hour === 0 && day === 0) {
     Rewards.findById(id)
       .then(result => updatedReward.expiryDate = result.currentTime)
       .then(() => {
-        return Rewards.findByIdAndUpdate({_id: id, parentId: req.user.id}, updatedReward, { new: true })
+        return Rewards.findByIdAndUpdate({ _id: id, parentId: req.user.id }, updatedReward, { new: true })
           .then(result => {
-              res.json(result);
+            res.json(result);
           })
           .catch(err => {
             next(err);
           });
-      });     
+      });
   } else {
-    Rewards.findByIdAndUpdate({_id: id, parentId: req.user.id}, updatedReward, { new: true })
-    .then(result => {
+    Rewards.findByIdAndUpdate({ _id: id, parentId: req.user.id }, updatedReward, { new: true })
+      .then(result => {
         res.json(result);
-    })
-    .catch(err => {
-      next(err);
-    });
+      })
+      .catch(err => {
+        next(err);
+      });
   }
 });
 
@@ -123,7 +175,7 @@ router.put('/:id', (req, res, next) => {
 
 router.delete('/:id', (req, res, next) => {
   const { id } = req.params;
-  Rewards.deleteOne({_id: id, parentId: req.user.id})
+  Rewards.deleteOne({ _id: id, parentId: req.user.id })
     .then(() => {
       res.status(204).end();
     })
