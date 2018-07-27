aadff7507fec4c27c84cd7233ec70ce96094732b
ArizonaMangoJuice <isaelwebdeveloper@gmail.com>
2018-07-26 21:27:09 -0600

    created helper folder and checkError file

---
+++ helper/checkErrors.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = function checkError(err){
+  if(err.code === 11000) {
+    err = new Error('The username already exists');
+    err.status = 400;
+    return err;
+  }
+  if (err.message === 'Parent validation failed: name: Path `name` is required.') {
+    err = new Error('name is required');
+    err.status = 400;
+    return err;
+  }
+  if (err.message === 'Child validation failed: name: Path `name` is required.') {
+    err = new Error('name is required');
+    err.status = 400;
+    return err;
+  }
+  if (err.message === 'Cast to ObjectId failed for value "5b5a309116e7f2763990427f4" at path "_id" for model "Child"') {
+    err = new Error('invalid id');
+    err.status = 400;
+    return err;
+  }
+};
--- routes/parent.js
+++ routes/parent.js
@@ -11,6 +11,7 @@ const Tasks = require('../models/tasks');
 const jwt = require('jsonwebtoken');
 const { JWT_SECRET, JWT_EXPIRY } = require('../config');
 
+const checkError = require('../helper/checkErrors');
 const router = express.Router();
 
 //move to helper folder
@@ -21,28 +22,7 @@ function createAuthToken(user) {
   });
 }
 
-function checkError(err){
-  if(err.code === 11000) {
-    err = new Error('The username already exists');
-    err.status = 400;
-    return err;
-  }
-  if (err.message === 'Parent validation failed: name: Path `name` is required.') {
-    err = new Error('name is required');
-    err.status = 400;
-    return err;
-  }
-  if (err.message === 'Child validation failed: name: Path `name` is required.') {
-    err = new Error('name is required');
-    err.status = 400;
-    return err;
-  }
-  if (err.message === 'Cast to ObjectId failed for value "5b5a309116e7f2763990427f4" at path "_id" for model "Child"') {
-    err = new Error('invalid id');
-    err.status = 400;
-    return err;
-  }
-}
+
 
 
 /* =================================================================================== */
